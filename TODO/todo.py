a272c6ff1e6b93de4452e274133c48f0bff02986
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-03-19 15:03:21 +0000

    This is my TODO list

--- TODO/todo.py
+++ TODO/todo.py
@@ -7,7 +7,7 @@ tasks = []
 
 @app.route("/")
 def index():
-    return render_template("index.html", tasks=tasks)
+    return render_template("index.html", tasks=tasks, enumerate=enumerate)
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -16,15 +16,15 @@ def add():
     tasks.append({"title": title, "description": description, "completed": False})
     return redirect(url_for("index"))
 
-@app.route("/complete/<int:index>")
-def complete(index):
-    tasks[index]["completed"] = True
+@app.route("/complete/<int:task_index>")
+def complete(task_index):
+    tasks[task_index]["completed"] = True
     return redirect(url_for("index"))
 
-@app.route("/delete/<int:index>")
-def delete(index):
-    tasks.pop(index)
+@app.route("/delete/<int:task_index>")
+def delete(task_index):
+    tasks.pop(task_index)
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
