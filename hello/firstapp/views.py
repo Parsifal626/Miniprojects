5cc17c02e26a2f100b3a5b643ddd04c53d2ad355
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-03-31 14:52:16 +0000

    2

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -4,4 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World!')
+    return HttpResponse("<h2> Главная </h2>")
+
+
+def about(request):
+    return HttpResponse("<h2> О сайте </h2>")
+
+def contact(request):
+    return HttpResponse("<h2> Контакты </h2>")
