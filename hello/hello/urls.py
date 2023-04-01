d059c3807d4c90b5bed6bdc7e5db5bfd68c245ff
Parsifal626 <625ruslan@gmail.com>
2023-04-01 13:49:11 +0300

    Changes

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path
 from firstapp import views
 
 urlpatterns = [
     path('', views.index, name ='home'),
-    path('about', views.about),
-    path('contact', views.contact),
     path("admin/", admin.site.urls),
+    re_path(r'^about', views.about),
+    re_path(r'^contact', views.contact),
+    re_path(r'^products/(?P<productid>\d+)/', views.products),
+    re_path(r'^users/(?P<id>\d+)/(?P<name>\D+)/', views.users), 
 ]
