0dff52e9f15aaf1f15ad01a8e8494a914651b8ed
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-04-04 13:08:04 +0000

    04.04.2023

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -20,9 +20,9 @@ from firstapp import views
 
 urlpatterns = [
     path('', views.index, name ='home'),
-    path("admin/", admin.site.urls),
     re_path(r'^about', views.about),
     re_path(r'^contact', views.contact),
-    re_path(r'^products/(?P<productid>\d+)/', views.products),
-    re_path(r'^users/(?P<id>\d+)/(?P<name>\D+)/', views.users), 
+    path("products/", views.products),
+    path('products/<int:productid>/', views.products),
+    path('users/', views.users),
 ]
