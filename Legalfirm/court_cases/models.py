c7105f824877bdda75ee92d8bafec48f33609d47
Parsifal626 <625ruslan@gmail.com>
2023-04-22 16:35:32 +0300

    court

--- Legalfirm/court_cases/models.py
+++ Legalfirm/court_cases/models.py
@@ -5,6 +5,7 @@ class Case(models.Model):
     case_number = models.CharField(max_length=50)
     case_title = models.CharField(max_length=200)
     date_filed = models.DateTimeField(default=timezone.now)
+    court = models.CharField(max_length=50)
     status = models.CharField(max_length=50)
 
     def __str__(self):
