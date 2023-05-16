32d7e74d6a58485937a47c8d7849094135e7e984
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-05-16 19:25:57 +0000

    1312

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 from rest_framework import generics
 # Create your views here.
+from serializer import WomenSerializer
 from .models import Women
+
 
 class WomenAPIview(generics.ListAPIView):
     queryset = Women.objects.all()
